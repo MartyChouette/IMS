dcfaa2d93a07c53e96d5d9f5a362ff66eb7ba9a7
Marty <92552832+MartyChouette@users.noreply.github.com>
2023-12-19 03:01:24 +0000

    some initila sql testing
     On branch main
     Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    modified:   src/python/main.py
    modified:   test_database.db

--- src/python/main.py
+++ src/python/main.py
@@ -1,13 +1,44 @@
 import product
 import sqlite3
-connection = sqlite3.connect('test_database.db')  # This creates a new file 'test_database.db'
+
+# Establish a connection and create a cursor
+connection = sqlite3.connect('test_database.db')
 cursor = connection.cursor()
-# You can now create tables and insert data
+
+# SQL command to create the table
+create_table_command = """
+CREATE TABLE IF NOT EXISTS products (
+    productID INTEGER PRIMARY KEY,
+    name TEXT,
+    price REAL,
+    quantity INTEGER
+);
+"""
+
+# Execute the command
+cursor.execute(create_table_command)
+
+# Commit the changes
+connection.commit()
 
 
 new_product = product.AddProduct(101, "Apple", 0.50, 100)
 
-print(f"New Product: ID={new_product.productID}, Name={new_product.name}, Price={new_product.price}, Quantity={new_product.qty}")
+#print(f"New Product: ID={new_product.productID}, Name={new_product.name}, Price={new_product.price}, Quantity={new_product.qty}")
+
+# Insert the product into the database
+insert_command = "INSERT INTO products (productID, name, price, quantity) VALUES (?, ?, ?, ?)"
+cursor.execute(insert_command, (new_product.productID, new_product.name, new_product.price, new_product.qty))
+
+# Retrieve and print all products
+cursor.execute("SELECT * FROM products")
+for row in cursor.fetchall():
+    print(row)
+
+# Close the connection
+cursor.close()
+connection.close()
+
 
 # Updating the product's name
 updated_product = product.UpdateProduct(new_product.productID, new_product.name, new_product.price, new_product.qty)
@@ -16,4 +47,4 @@ updated_product = product.UpdateProduct(new_product.productID, new_product.name,
 updated_product.update_name("Green Apple")
 
 # Print updated product details
-print(f"Updated Product: ID={updated_product.productID}, Name={updated_product.name}, Price={updated_product.price}, Quantity={updated_product.qty}")
+#print(f"Updated Product: ID={updated_product.productID}, Name={updated_product.name}, Price={updated_product.price}, Quantity={updated_product.qty}")
